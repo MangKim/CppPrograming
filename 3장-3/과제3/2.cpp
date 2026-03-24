eebea3072a1b7fe4ce890c1a470d084607dff315
MinjuneKim <128655130+MangKim@users.noreply.github.com>
2026-03-24 20:18:36 +0900

    Add files via upload

--- 3장-3/과제3/2.cpp
+++ 3장-3/과제3/2.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
 class Triangle {
 private:
-    double width;
-    double height;
-
+    int width, height;
 public:
-    Triangle(double w, double h) {
+    Triangle(int w = 1, int h = 1) {
         width = w;
         height = h;
         cout << "밑변 " << width << " 높이 " << height << "인 삼각형 생성" << endl;
     }
-
     ~Triangle() {
         cout << "밑변 " << width << " 높이 " << height << "인 삼각형 소멸" << endl;
     }
-
     double getArea() {
         return (width * height) / 2.0;
     }
@@ -26,10 +23,9 @@ Triangle tri1(4, 8);
 Triangle tri2(2, 2);
 
 int main() {
-    system("chcp 65001 > nul");
-
-    cout << "삼각형의 면적은 " << tri1.getArea() << endl;
+    SetConsoleOutputCP(CP_UTF8);
     cout << "삼각형의 면적은 " << tri2.getArea() << endl;
+    cout << "삼각형의 면적은 " << tri1.getArea() << endl;
 
     return 0;
 }
